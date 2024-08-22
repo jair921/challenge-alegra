c0924334a699737aac4e228048225b92b9728a6e
Jair Andres Martinez <martinez.jair8@gmail.com>
2024-08-22 14:18:41 -0500

    feat: forward order service

--- src/gateway/routes/api.php
+++ src/gateway/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,5 +11,11 @@ Route::middleware('auth:api')->prefix("v1")->group(function () {
     });
 
     //Routes to microservices
+
+    // Endpoint para crear una orden
+    Route::post('/orders', [OrderController::class, 'createOrder']);
+
+    // Endpoint para listar órdenes
+    Route::get('/orders', [OrderController::class, 'listOrders']);
 
 });
